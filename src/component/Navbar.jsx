c8ec2d898673f8b16268d8ce6edcd9e1017a8e4a
Ritik <ritikrana4@gmail.com>
2020-04-09 01:58:40 +0530

    added navbar icons

--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -1,6 +1,10 @@
 import React from 'react';
 import {Link,NavLink} from 'react-router-dom';
 
+import home from '../assets/home.png';
+import info from '../assets/info.png';
+import learn from '../assets/book-open.png';
+import test from '../assets/briefcase.png';
 import '../styles/navbar.style.css';
 
 const Navbar = () => {
@@ -8,10 +12,11 @@ const Navbar = () => {
   
             <div className='navbar'>
                 <div className='navbar-box'>
-                     <div ><NavLink to='home'>Home</NavLink></div>
-                     <div ><NavLink to='info'>Info</NavLink></div>
-                     <div ><NavLink to='learn'>Learn</NavLink></div>
-                     <div ><NavLink to='testcenters'>TestCenters</NavLink></div>
+                    <NavLink to='home'>
+                         <div className='grp'><img src={home}/><h6>Home</h6></div></NavLink>
+                     <div ><NavLink to='info'><div className='grp'><img src={info}/><h6>Info</h6></div></NavLink></div>
+                     <div ><NavLink to='learn'><div className='grp'><img src={learn}/><h6>Learn</h6></div></NavLink></div>
+                     <div ><NavLink to='testcenters'><div className='grp'><img src={test}/><h6>TestCenters</h6></div></NavLink></div>
                 </div>
              
             </div>
