5b3940e168dc4f524dff068732b49e9091cc62c3
Ritik <ritikrana4@gmail.com>
2020-04-11 04:19:50 +0530

    minor fixes

--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -7,18 +7,21 @@ import learn from '../assets/book-open.png';
 import test from '../assets/briefcase.png';
 import '../styles/navbar.style.css';
 
+
+
 const Navbar = () => {
+    let i;
     return(
   
             <div className='navbar'>
                 <div className='navbar-box'>
-                    <NavLink to='home'>
-                         <div className='grp'><img alt='home' src={home}/><h6>Home</h6></div></NavLink>
-                     <div ><NavLink to='info'><div className='grp'><img alt='info' src={info}/><h6>Info</h6></div></NavLink></div>
-                     <div ><NavLink to='learn'><div className='grp'><img alt='learn' src={learn}/><h6>Learn</h6></div></NavLink></div>
-                     <div ><NavLink to='testcenters'><div className='grp'><img alt='testcenters' src={test}/><h6>TestCenters</h6></div></NavLink></div>
+                
+                     <div><NavLink activeStyle={{ color: "#00BFFF"}} to='home'><div className='grp'><img className="image" alt='home' src={home}/><h6>Home</h6></div></NavLink></div>
+                     <div ><NavLink  activeStyle={{ color: "#00BFFF"}} to='info'><div className='grp'><img alt='info' src={info}/><h6>Info</h6></div></NavLink></div>
+                     <div ><NavLink  activeStyle={{ color: "#00BFFF"}} to='learn'><div className='grp'><img alt='learn' src={learn}/><h6>Learn</h6></div></NavLink></div>
+                     <div ><NavLink  activeStyle={{ color: "#00BFFF"}} to='testcenters'><div className='grp'><img alt='testcenters' src={test}/><h6>TestCenters</h6></div></NavLink></div>
                 </div>
-             
+            
             </div>
     
     )
--- src/pages/Learn.jsx
+++ src/pages/Learn.jsx
@@ -1,13 +1,10 @@
 import React from 'react';
 
-import img_1 from '../assets/do-1.png'; 
-import img_2 from '../assets/do-2.png'; 
-import img_3 from '../assets/do-3.png'; 
-import img_4 from '../assets/do-4.png'; 
-import img_5 from '../assets/do-5.png'; 
-import img_6 from '../assets/do-6.png'; 
 
-import '../styles/learn.style.css'
+import doos from '../assets/doos.png';
+import donts from '../assets/donts.png';
+
+import '../styles/learnn.style.css';
 
 const Learn =()=>{
     return(
@@ -19,68 +16,10 @@ const Learn =()=>{
                 
                 
                 <div className='section-2'>
-                    <div className='title'>Do's</div>
-                    <div className='grp-out'>
-                        <div><img alt='img' src={img_1}/></div>
-                        <div className='grp-in'>
-                            <div className='grp-title'>Wear Masks</div>
-                            <div className='grp-para'>Helps you in filtering out very <br/> small 
-                             particles that could <br/>possibly  containCoronavirus.
-                             </div>
-                        </div>
-                    </div>
-                    <div className='grp-out'>
-                        <div><img alt='img' src={img_2}/></div>
-                        <div className='grp-in'>
-                            <div className='grp-title'>Wash your Hands Often</div>
-                            <div className='grp-para'>Washing your hands very often <br/>will 
-                                decrease the chances of <br/> Virus entering
-                                your body.
-                             </div>
-                        </div>
-                    </div>
-                    <div className='grp-out'>
-                        <div><img alt='img' src={img_3}/></div>
-                        <div className='grp-in'>
-                            <div className='grp-title'>Maintain Social Distancing</div>
-                            <div className='grp-para'>Maintaining social distancing <br/>can 
-                            help us all get rid of this <br/>  pandemic sooner.
-                             </div>
-                        </div>
-                    </div>
+                    <img src={doos} alt="do's image" />
                 </div>
                 <div className='section-3'>
-                    <div className='title'>Dont's</div>
-                    <div className='grp-out'>
-                        
-                        <div className='grp-in'>
-                            <div className='grp-title'>Don’t spread Fake News</div>
-                            <div className='grp-para'>Don’t forward anything that is
-                            <br/>not coming from official sources.</div>
-                        </div>
-                        <div className='image'><img alt='img' src={img_4}/></div>
-
-                    </div>
-                    <div className='grp-out'>
-                        
-                        <div className='grp-in'>
-                            <div className='grp-title'>Don’t Panic</div>
-                            <div className='grp-para'>Panicking won’t help, Taking
-                                                <br/>Precautions will.
-                             </div>
-                        </div>
-                        <div className='image'><img alt='img' src={img_5}/></div>
-                    </div>
-                    <div className='grp-out'>
-                        
-                        <div className='grp-in'>
-                            <div className='grp-title'>Don’t Go Out</div>
-                            <div className='grp-para'>It’s that once in a lifetime chance<br/>
-                                that you can saved the world <br/>while sitting on your couch.<br/> Do that.</div>
-                        </div>
-                        <div className='image'><img alt='img' src={img_6}/></div>
-
-                    </div>
+                    <img src={donts} alt="dont's image" />
                 </div>
         </div>
     )
